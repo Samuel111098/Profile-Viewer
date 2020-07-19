d3c4782eaf3a95d76744a4b5188be07f1f937cc5
Samuel S Ravella <samuelravella11@gmail.com>
2020-07-19 23:48:17 +0530

    Basic AuthO
    Authorization to use Github API

--- user-profile-viewer/src/components/users/Users.js
+++ user-profile-viewer/src/components/users/Users.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UserItem from './UserItem';
+import Spinner from '../layout/Spinner';
 import './User.css';
 
-class Users extends Component {
-	render() {
-		return (
-			<div className='userStyle'>{this.props.users.map((user) => <UserItem key={user.id} user={user} />)}</div>
-		);
+const Users = ({ users, loading }) => {
+	if (loading) {
+		return <Spinner />;
+	} else {
+		return <div className='userStyle'>{users.map((user) => <UserItem key={user.id} user={user} />)}</div>;
 	}
-}
+};
 
 export default Users;
